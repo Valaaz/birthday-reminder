10d45a81ed3e193dc5902c4e1290af41e3dc5ab7
Valaaz <val.zancoth@gmail.com>
2022-07-03 14:54:58 +0200

    init project

---
+++ birthday_reminder/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Birthday reminder',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Birthday reminder'),
+          backgroundColor: Colors.purple,
+        ),
+        body: const Center(
+          child: Text('Hello'),
+        ),
+      ),
+    );
+  }
+}
